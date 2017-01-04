a19423c8c3967a1e94d4763360d778e7d8c4281d
Darlan Mendonça <darlanmendonca@gmail.com>
2017-01-04 13:17:49 -0200

    add some specs to type and set value

--- sources/scripts/mn-input.spec.js
+++ sources/scripts/mn-input.spec.js
@@ -1,26 +1,33 @@
 import {expect} from 'chai'
 
 fixture `mn-input`
-  .page('http://localhost:3000')
-  .beforeEach(async page => await page.wait(5000))
+  .page('http://localhost:3000/demo.html')
+  // .beforeEach(async page => await page.wait(5000))
 
-test('type username', async page => {
-  await page.typeText('#username', 'Darlan Mendonça')
-  const input = await page.select('#username')
+test('type value', async page => {
+  await page.typeText('#username', 'John Snow')
+  await page.typeText('#password', 'loveygrit')
+  await page.typeText('#email', 'john@snow.com')
 
-  expect(input.value).to.equal('Darlan Mendonça')
+  const username = await page.select('#username')
+  const password = await page.select('#password')
+  const email = await page.select('#email')
+
+  expect(username.value).to.equal('John Snow')
+  expect(password.value).to.equal('loveygrit')
+  expect(email.value).to.equal('john@snow.com')
 })
 
-test('type password', async page => {
-  await page.typeText('#password', 'loremipsum')
-  const input = await page.select('#password')
+test('set value', async page => {
+  const username = await page.select('#username')
+  const password = await page.select('#password')
+  const email = await page.select('#email')
 
-  expect(input.value).to.equal('loremipsum')
-})
+  username.value = 'John Snow'
+  password.value = 'loveygrit'
+  email.value = 'john@snow.com'
 
-test('type email', async page => {
-  await page.typeText('#email', 'darlanmendonca@gmail.com')
-  const input = await page.select('#email')
-
-  expect(input.value).to.equal('darlanmendonca@gmail.com')
+  expect(username.value).to.equal('John Snow')
+  expect(password.value).to.equal('loveygrit')
+  expect(email.value).to.equal('john@snow.com')
 })
